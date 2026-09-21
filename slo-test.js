3ce628cac668d34301e0e97daf05c706c3962050
Oyunjargal <jargal130613@gmail.com>
2026-09-22 02:53:56 +0800

    /report threshold нэмэн туршив.

--- slo-test.js
+++ slo-test.js
@@ -4,10 +4,11 @@ import { check, sleep } from 'k6';
 export const options = {
     vus: 20, duration: '1m',
     thresholds: {
-        'http_req_duration{name:cart}': ['p(95)<50'], // Performance SLO
-        'http_req_failed{name:pay}': ['rate<0.08'], // Reliability SLO
-        'checks': ['rate>0.90'], // Availability SLO
-    },
+    'http_req_duration{name:cart}': ['p(95)<50'],
+    'http_req_failed{name:pay}': ['rate<0.08'],
+    'http_req_duration{name:report}': ['p(95)<450'],
+    'checks': ['rate>0.90'],
+},
 };
 
 export default function () {
